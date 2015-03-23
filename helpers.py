1c4dc060dee6e51ee1d65e6af92e05c865953ce8
iver56 <iver56@hotmail.com>
2015-03-23 16:47:19 +0100

    Refactor larger_than method

--- helpers.py
+++ helpers.py
@@ -49,8 +49,6 @@ def absolute_diff(former_list, current_list):
     diff = map(sub, current_list, former_list)
     return map(abs, diff)
 
-def larger_than(pixel_list,number):
-    if max(pixel_list) > number:
-        return True
-    else:
-        return False
+
+def is_max_larger_than(pixel_list, number):
+    return max(pixel_list) > number
